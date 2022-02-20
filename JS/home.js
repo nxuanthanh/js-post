80f999b1431a09a34a859b254c5027565fa3b00d
nxuanthanh <n.xuanthanhh@gmail.com>
2022-02-20 16:12:40 +0700

    Pagination  - disable page links  - refecth data on page chage

--- JS/home.js
+++ JS/home.js
@@ -1,10 +1,12 @@
 import postApi from './api/postAPI';
-import { setTextContent, truncateText } from './utils';
+import { getUlPagination, setTextContent, truncateText } from './utils';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import debounce from 'lodash.debounce';
 
 // to use fromNow func
 dayjs.extend(relativeTime);
+
 function createPostItem(post) {
   if (!post) return;
 
@@ -37,10 +39,12 @@ function createPostItem(post) {
 }
 
 function renderPostList(postList) {
-  if (!Array.isArray(postList) || postList.length === 0) return;
+  if (!Array.isArray(postList)) return;
 
   const ulElement = document.getElementById('postList');
   if (!ulElement) return;
+
+  ulElement.textContent = '';
 
   postList.forEach((post) => {
     const liElement = createPostItem(post);
@@ -50,26 +54,50 @@ function renderPostList(postList) {
   });
 }
 
-function handleFilterChange(filterName, filterValue) {
-  // update query params
-
-  const url = new URL(window.location);
-  url.searchParams.set(filterName, filterValue);
-  history.pushState({}, '', url);
-
-  // fetch API
-  // re-render postlist
-}
-
 function handlePrevClick() {
-  console.log('vnd');
+  const ulPagination = getUlPagination();
+  if (!ulPagination) return;
+
+  const page = Number.parseInt(ulPagination.dataset.page) || 1;
+  if (page <= 1) return;
+
+  handleFilterChange('_page', page - 1);
 }
 function handleNextClick() {
-  console.log('usd');
+  const ulPagination = getUlPagination();
+  if (!ulPagination) return;
+
+  const page = Number.parseInt(ulPagination.dataset.page) || 1;
+  const totalPages = Number.parseInt(ulPagination.dataset.totalPages);
+  if (page >= totalPages) return;
+
+  handleFilterChange('_page', page + 1);
+}
+
+function renderPagination(pagination) {
+  const ulPagination = getUlPagination();
+
+  if (!pagination || !ulPagination) return;
+
+  // calc totalPages
+  const { _page, _limit, _totalRows } = pagination;
+  const totalPages = Math.ceil(_totalRows / _limit);
+
+  // save page and totalPages to ulPagination
+  ulPagination.dataset.page = _page;
+  ulPagination.dataset.totalPages = totalPages;
+
+  // check enable/disaple in prev link
+  if (_page <= 1) ulPagination?.firstElementChild.classList.add('disabled');
+  else ulPagination?.firstElementChild.classList.remove('disabled');
+
+  // check enable/disaple in next link
+  if (_page >= 5) ulPagination?.lastElementChild.classList.add('disabled');
+  else ulPagination?.lastElementChild.classList.remove('disabled');
 }
 
 function initPagination() {
-  const ulPagination = document.getElementById('pagination');
+  const ulPagination = getUlPagination();
   if (!ulPagination) return;
 
   const prevElement = ulPagination.firstElementChild?.firstElementChild;
@@ -93,21 +121,63 @@ function initURL() {
   const url = new URL(window.location);
 
   // update search params if needed
-
   if (!url.searchParams.get('_page')) url.searchParams.set('_page', 1);
   if (!url.searchParams.get('_limit')) url.searchParams.set('_limit', 6);
 
   history.pushState({}, '', url);
 }
 
+function initSearch() {
+  const inputSearch = document.getElementById('inputSearch');
+  if (!inputSearch) return;
+
+  const queryParams = new URLSearchParams(window.location.search);
+  if (queryParams.get('title_like')) {
+    inputSearch.value = queryParams.get('title_like');
+  }
+
+  const debounceSearch = debounce((e) => handleFilterChange('title_like', e.target.value), 500);
+
+  // set default value from query params
+  inputSearch.addEventListener('input', debounceSearch);
+}
+
+async function handleFilterChange(filterName, filterValue) {
+  try {
+    // update query params
+    const url = new URL(window.location);
+    url.searchParams.set(filterName, filterValue);
+
+    // reset page if needed
+    if (filterName === 'title_like') url.searchParams.set('_page', 1);
+    history.pushState({}, '', url);
+
+    // fetch API
+    // re-render postlist
+
+    const { data, pagination } = await postApi.getALL(url.searchParams);
+    renderPostList(data);
+    renderPagination(pagination);
+  } catch (error) {
+    console.log('failed to fetch postlist', error);
+  }
+}
+
 (async () => {
   try {
+    // attach click event for links
     initPagination();
-    initURL();
-    const queryParams = new URLSearchParams(window.location.search);
+    initSearch();
 
+    // set default pagination {_page, _limit} on URL
+    initURL();
+
+    // render postlist based URL params
+    const queryParams = new URLSearchParams(window.location.search);
     const { data, pagination } = await postApi.getALL(queryParams);
+
     renderPostList(data);
+    renderPagination(pagination);
   } catch (error) {
     console.log('get all failed', error);
   }
